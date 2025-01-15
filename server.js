9b360d54ee913c9c65ec163f720d8e562cd676d7
hazardzone <tahri.mohamed.hamza@gmail.com>
2025-01-15 11:34:51 +0100

    second commit started the next app

--- server.js
+++
@@ -1,9 +0,0 @@
-const handleSubscribe = async (e) => {
-  e.preventDefault();
-  const response = await fetch("/api/subscribe", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email: userEmail }),
-  });
-  if (response.ok) alert("Subscription successful!");
-};
